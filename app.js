fba9e0f54e2efafc0d49aef8c0c3ffe10ff21ccc
theleonmo <le.mo@live.co.uk>
2022-06-21 20:27:21 +0100

    installed and imported bodyparser changed main from indexjs to appjs in packagejson

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 import express from "express";
-
+import bodyParser from "body-parser" 
 import router from "./routes/tickets.js"
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-// app.get("/api", (req, res) => {
-//   res.json({ message: "Hi" });
-// });
+app.use("/tickets", router);
 
-    app.use(express.json());
+app.use(function (req, res, next) {
+res
+    .status(404)
+    .json({ message: "We couldn't find what you were looking for 😞" });
+});
 
-
-    app.use("/tickets", router);
-
-    app.use(function (req, res, next) {
-    res
-        .status(404)
-        .json({ message: "We couldn't find what you were looking for 😞" });
-    });
-
-    app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).json(err);
-    });
+app.use(function (err, req, res, next) {
+console.error(err.stack);
+res.status(500).json(err);
+});
 
 
 
--- plan.md
+++ plan.md
@@ -9,7 +9,7 @@
 
 <!-- 2. Install packages : express, pg, nodemon, dotenv -->
 
-3. Sort imports/exports
+<!-- 3. Sort imports/exports
 
 4. Set up package syntax 
 
@@ -17,8 +17,11 @@
 
 6. Build routes
 
-7. Build models
+7. Build models -->
 
 8. Construct and populate tables
 
 9. Link up to front end
+
+1. Backend package.json - changed 'main' from index.js to app.js
+2. Frontend package.json - added 'proxy'
