26aeeec9802de78d7fa2f74906653e978774bf44
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-07-16 14:47:26 +0100

    removed header function from app.js

--- app.js
+++ app.js
@@ -11,21 +11,6 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-app.use(function (req, res, next) {
-	res.header(
-		"Access-Control-Allow-Origin",
-		"https://giraffe-support.netlify.app/"
-	);
-	res.header(
-		"Access-Control-Allow-Headers",
-		"Origin, X-Requested-With, Content-Type, Accept"
-	);
-	next();
-});
-
-
-
 app.use("/tickets", router);
 
 app.use(function (req, res, next) {
