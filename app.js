478488b22e21f86c632d8b99963772cf6448d032
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-07-16 13:55:51 +0100

    revert back

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/tickets.js";
-import cors from "cors";
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
