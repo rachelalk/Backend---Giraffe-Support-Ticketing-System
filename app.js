ba999bf45e3c432cbd658972c5bb277d766858e5
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-07-16 13:19:53 +0100

    undo previous change

--- app.js
+++ app.js
@@ -7,15 +7,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cors());
-app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/tickets", router);
 
